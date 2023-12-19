3f20d78187cb4d904c281fbd379d4f40e06200ed
mickey-40 <mickeyarnold53@yahoo.com>
2023-12-18 20:43:04 -0600

    trouble shooting javascript

--- script.js
+++ script.js
@@ -1,20 +1,65 @@
-const btn = document.querySelector('button');
-const main = document.querySelector('main');
+// const btn = document.querySelector('btn');
+// const main = document.querySelector('main');
+// const results = document.querySelector('result');
 
-btn.addEventListener('click', function palcheck(){
-  var word = document.getElementById('word').value;
-  // console.log(word);
-  var revword = "";
-  for(let i = word.length-1; i >= 0; i--){
-    revword += word[i];
-  }
-  // console.log(revword);
-  if( word === revword){
-    console.log(true);
-    document.body.style.backgroundColor = '#00FF00';
+
+// btn.addEventListener('click', palcheck);
+
+// function palcheck(){
+//   const word = document.querySelector('input-text').value;
+//   var revword = reversedWord(word);
+//   console.log(revword)
+//   if( word.value === revword){
+//   //   changeBackground(true,word);
+//     console.log(word)
+//     results.innerHTML = `${word} is a palindrome`;
+//   } else {
+  //   changeBackground(false,word);
+  //   // results.innerHTML = "is not a palindrome";
+  // console.log(word.value)
+  // }
+
+// }
+
+// function reversedWord (word){
+//   let reverse = ""
+//   for(let i = word.length-1; i >= 0; i--){
+//     reverse += word[i];
+//   }
+//   return reverse;
+// }
+// function changeBackground(x,word){
+//   if(x){
+//     document.body.style.backgroundColor = '#00FF00';
+//     results.innerHTML = `${word} is a palindrome`;
+//   } else {
+//     document.body.style.backgroundColor = '#FF0000';
+//     results.innerHTML = `${word} is a palindrome`;
+
+//   }
+// }
+
+const btn = document.querySelector(".btn");
+const result = document.querySelector(".result");
+
+btn.addEventListener("click", palindrome);
+
+// madam
+// noon
+function palindrome() {
+  const word = document.querySelector(".input-text").value;
+  let len = word.length;
+
+  let start = word.substring(0, Math.floor(len / 2)).toLowerCase();
+  let end = word.substring(len - Math.floor(len / 2)).toLowerCase();
+
+  // let flip = end.split("").reverse().join("");
+
+  let flip = [...end].reverse().join("");
+  if (start == flip) {
+    result.innerHTML = `${word.toUpperCase()} is a palindrome`;
   } else {
-    console.log(false);
-    document.body.style.backgroundColor = '#FF0000'
+    result.innerHTML = `${word.toUpperCase()} is NOT a palindrome`;
   }
-
-})
+  // alert(flip);
+}
